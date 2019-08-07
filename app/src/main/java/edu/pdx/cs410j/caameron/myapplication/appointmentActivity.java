86760813ba1f89f3ef1470e6b0f4f7fb3d83633f
caameron <30531788+caameron@users.noreply.github.com>
2019-08-06 17:05:08 -0700

    make an appointment works

--- app/src/main/java/edu/pdx/cs410j/caameron/myapplication/appointmentActivity.java
+++ app/src/main/java/edu/pdx/cs410j/caameron/myapplication/appointmentActivity.java
@@ -30,6 +30,37 @@ public class appointmentActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         Log.v("print", submit.getText().toString() + owner.getText().toString());
+                        Appointment appt = null;
+                        try
+                        {
+                            String beginTime = start.getText().toString();
+                            String endTime = end.getText().toString();
+                            String desciptionAppt = descripion.getText().toString();
+                            String ownerAppt = owner.getText().toString();
+                            Log.v("Print", beginTime);
+
+                            String [] begSplit = beginTime.split(" ");
+                            String [] endSplit = beginTime.split(" ");
+                            if(begSplit.length != 3 || endSplit.length != 3)
+                            {
+                                throw new Exception("Date formatted incorrectly must be of the form mm/dd/yyyy hh:mm am/pm");
+                            }
+                            String beginD = begSplit[0];
+                            String beginT = begSplit[1];
+                            String beginTimeOfDay = begSplit[2];
+                            String endD = endSplit[0];
+                            String endT = endSplit[1];
+                            String endTimeOfDay = endSplit[2];
+                            appt = new Appointment(beginD, beginT, endD, endT, desciptionAppt, beginTimeOfDay, endTimeOfDay);
+                            Log.v("Print", appt.toString());
+
+                        }
+                        catch (Exception err)
+                        {
+                            Log.v("print", err.getMessage());
+                        }
+
+
 
                     }
                     }
